b1491bf0bac69790ce7b40e462830a0a3db65767
brandonwatson <blwatson@gmail.com>
2024-05-20 13:27:22 -0600

    added backend json, add delete functionality, added auth.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+//Amplify imports
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -21,6 +25,11 @@ export default function App() {
     });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   useEffect(() => {
     listTodos();
   }, []);
@@ -32,21 +41,27 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator>{({signOut, user}) => (
+      <main>
+        <h1>Todos for {user?.signInDetails?.loginId.toUpperCase()}</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+            <li
+              onClick={() => deleteTodo(todo.id)}
+              key={todo.id}
+            >{todo.content}</li>
+          ))}
+        </ul>
+        <div>
+          🥳 App successfully hosted. Try creating a new todo.
+          <br />
+          <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+            Review next steps of this tutorial.
+          </a>
+        </div>
+        <button onClick={signOut}>Sign Out</button>
+      </main>
+    )}</Authenticator>
   );
 }
